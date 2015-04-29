d8c980e5a773d02580276bd33d2aaf9b5e6038a8
Mark Adams <mark.adams86@gmail.com>
2015-04-29 15:07:59 -0700

    Standardize light text color

--- Tropos/Sources/Controllers/AppearanceController.swift
+++ Tropos/Sources/Controllers/AppearanceController.swift
@@ -2,10 +2,12 @@ import UIKit
 import TroposCore
 
 @objc(TRAppearanceController) final class AppearanceController: NSObject {
-  static func configureAppearance() {
-    UINavigationBar.appearance().titleTextAttributes = [NSFontAttributeName: UIFont.defaultLightFont(size: 20), NSForegroundColorAttributeName: UIColor.lightTextColor()]
-    
-    let attributes = [NSFontAttributeName: UIFont.defaultLightFont(size: 16), NSForegroundColorAttributeName: UIColor.lightTextColor()]
-    UIBarButtonItem.appearance().setTitleTextAttributes(attributes, forState: .Normal)
-  }
+    static func configureAppearance() {
+        let navigationBar = UINavigationBar.appearance()
+        navigationBar.titleTextAttributes = [NSFontAttributeName: UIFont.defaultLightFont(size: 20), NSForegroundColorAttributeName: UIColor.lightTextColor()]
+        navigationBar.tintColor = .lightTextColor()
+
+        let attributes = [NSFontAttributeName: UIFont.defaultLightFont(size: 17), NSForegroundColorAttributeName: UIColor.lightTextColor()]
+        UIBarButtonItem.appearance().setTitleTextAttributes(attributes, forState: .Normal)
+    }
 }
