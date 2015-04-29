2b5e53396cb22c6030b313be0b43ef813ebc125c
Mark Adams <mark.adams86@gmail.com>
2015-04-29 15:05:34 -0700

    Finish transition from ultralight to light font

--- Tropos/Sources/Controllers/AppearanceController.swift
+++ Tropos/Sources/Controllers/AppearanceController.swift
@@ -3,9 +3,9 @@ import TroposCore
 
 @objc(TRAppearanceController) final class AppearanceController: NSObject {
   static func configureAppearance() {
-    UINavigationBar.appearance().titleTextAttributes = [NSFontAttributeName: UIFont.defaultUltraLightFont(size: 20), NSForegroundColorAttributeName: UIColor.lightTextColor()]
+    UINavigationBar.appearance().titleTextAttributes = [NSFontAttributeName: UIFont.defaultLightFont(size: 20), NSForegroundColorAttributeName: UIColor.lightTextColor()]
     
-    let attributes = [NSFontAttributeName: UIFont.defaultUltraLightFont(size: 16), NSForegroundColorAttributeName: UIColor.lightTextColor()]
+    let attributes = [NSFontAttributeName: UIFont.defaultLightFont(size: 16), NSForegroundColorAttributeName: UIColor.lightTextColor()]
     UIBarButtonItem.appearance().setTitleTextAttributes(attributes, forState: .Normal)
   }
 }
